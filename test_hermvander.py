021bac58b9ac556614bb2ed6c86c59d8827a9ad6
Ziyao Zhang <ziyaointl@gmail.com>
2019-12-23 15:32:15 -0800

    Add untested version of hermvander

---
+++ test_hermvander.py
@@ -0,0 +1,44 @@
+import cupy as cp
+from numba import cuda
+import numpy as np
+import numba, pytest
+from time import time
+from astropy.table import Table
+
+@cuda.jit
+def hermvander(x, deg, output_matrix):
+    i = cuda.grid(1)
+    stride = cuda.gridsize(1)
+    for i in range(i, x.shape[0], stride):
+        output_matrix[i][0] = 1
+        if deg > 0:
+            output_matrix[i][1] = x[i] * 2 # x2 is always stored in output_matrix[i][1]
+            for j in range(2, deg + 1):
+                output_matrix[i][j] = output_matrix[i][j - 1] * output_matrix[i][1] \
+                                    - output_matrix[i][j - 2] * 2 * (i - 1)
+
+def hermvander_wrapper(x, deg):
+    """Temprorary wrapper the allocates memory and calls hermvander_gpu
+    """
+    output = cp.ndarray((len(x), deg + 1))
+    blocksize = 256
+    numblocks = (len(x) + blocksize - 1) // blocksize
+    hermvander[numblocks, blocksize](x, deg, output)
+    return output
+
+def test_hermvander():
+    # Generate dummy input
+    degree = 10
+    np.random.seed = 1
+    x_cpu = np.random.rand(100)
+    x_gpu = cp.array(x_cpu)
+
+    # Calculate on cpu
+    hermvander_cpu = np.hermvander(x, degree)
+
+    # Calculate on gpu
+    hermvander_gpu = hermvander_wrapper(x_gpu, degree)
+
+    # Compare
+    assert np.allclose(legvander_cpu, legvander_gpu.get())
+
