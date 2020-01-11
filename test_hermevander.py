5f373bf73b076445aaa9e6a671d5a934f2f8f3a7
Ziyao Zhang <ziyaointl@gmail.com>
2020-01-11 00:11:54 -0800

    Adapt hermevander for 2 dimensional inputs

--- test_hermevander.py
+++ test_hermevander.py
@@ -7,23 +7,25 @@ from astropy.table import Table
 
 @cuda.jit
 def hermevander(x, deg, output_matrix):
-    i = cuda.grid(1)
-    stride = cuda.gridsize(1)
-    for i in range(i, x.shape[0], stride):
-        output_matrix[i][0] = 1
+    i, j = cuda.grid(2)
+    _, stride = cuda.gridsize(2)
+    for j in range(j, x.shape[1], stride):
+        output_matrix[i][j][0] = 1
         if deg > 0:
-            output_matrix[i][1] = x[i]
-            for j in range(2, deg + 1):
-                output_matrix[i][j] = output_matrix[i][j-1]*x[i] - output_matrix[i][j-2]*(j-1)
+            output_matrix[i][j][1] = x[i][j]
+            for k in range(2, deg + 1):
+                output_matrix[i][j][k] = output_matrix[i][j][k-1]*x[i][j] - output_matrix[i][j][k-2]*(k-1)
 
 def hermevander_wrapper(x, deg):
-    """Temprorary wrapper the allocates memory and calls hermevander_gpu
+    """Temprorary wrapper that allocates memory and calls hermevander_gpu
     """
-    output = cp.ndarray((len(x), deg + 1))
+    if x.ndim == 1:
+        x = cp.expand_dims(x, 0)
+    output = cp.ndarray(x.shape + (deg+1,))
     blocksize = 256
-    numblocks = (len(x) + blocksize - 1) // blocksize
+    numblocks = (x.shape[0], (x.shape[1] + blocksize - 1) // blocksize)
     hermevander[numblocks, blocksize](x, deg, output)
-    return output
+    return cp.squeeze(output)
 
 def test_hermevander():
     # Generate dummy input
@@ -37,6 +39,8 @@ def test_hermevander():
 
     # Calculate on gpu
     hermevander_gpu = hermevander_wrapper(x_gpu, degree)
+
+    print(hermevander_cpu, hermevander_gpu.get())
 
     # Compare
     assert np.allclose(hermevander_cpu, hermevander_gpu.get())
