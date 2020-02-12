6874e57c74fce542ad75554c9402a6cb67ae4212
Laurie Stephey <lastephey@lbl.gov>
2020-02-11 23:58:20 -0800

    remove desi_libs.sh, now lives on /global/common

--- desi_libs.sh
+++
@@ -1,8 +0,0 @@
-#desispec
-export PYTHONPATH=/global/cscratch1/sd/stephey/git_repo/desispec/py:$PYTHONPATH
-export PATH=/global/cscratch1/sd/stephey/git_repo/desispec/bin:$PATH
-
-#desiutil
-export PYTHONPATH=/global/cscratch1/sd/stephey/git_repo/desiutil/py:$PYTHONPATH
-export PATH=/global/cscratch1/sd/stephey/git_repo/desiutil/bin:$PATH
-
