a2f28bff9860a6a93f371686e4332e59ad78fe5f
Daniel Margala <djmargala@gmail.com>
2020-06-02 11:23:52 -0700

    add a few nvtx markers

--- py/gpu_specter/extract/both.py
+++ py/gpu_specter/extract/both.py
@@ -9,6 +9,15 @@ from ..util import Timer
 from ..util import get_array_module
 from .cpu import get_spec_padding
 
+
+def safe_range_push(xp, name):
+    if xp.__name__ == 'cupy':
+        xp.cuda.nvtx.RangePush(name)
+
+def safe_range_pop(xp):
+    if xp.__name__ == 'cupy':
+        xp.cuda.nvtx.RangePop()
+
 def xp_deconvolve(pixel_values, pixel_ivar, A):
     """Calculate the weighted linear least-squares flux solution for an observed trace.
 
@@ -26,9 +35,11 @@ def xp_deconvolve(pixel_values, pixel_ivar, A):
     assert xp == get_array_module(pixel_values)
     assert xp == get_array_module(pixel_ivar)
     #- Set up the equation to solve (B&S eq 4)
+    safe_range_push(xp, 'BS Eq 4 Setup')
     ATNinv = A.T.dot(xp.diag(pixel_ivar))
     iCov = ATNinv.dot(A)
     iCov += 1e-12*xp.eye(iCov.shape[0])
+    safe_range_pop(xp)
     #- Solve the linear least-squares problem.
     #- Force rcond to be the same when using cupy/numpy 
     #- See: https://github.com/numpy/numpy/blob/v1.18.4/numpy/linalg/linalg.py#L2247
@@ -36,7 +47,9 @@ def xp_deconvolve(pixel_values, pixel_ivar, A):
     # deconvolved, res, rank, sing = xp.linalg.lstsq(iCov, ATNinv.dot(pixel_values), rcond=rcond)
     # if rank < len(deconvolved):
     #     print('WARNING: deconvolved inverse-covariance is not positive definite.')
+    safe_range_push(xp, 'BS Eq 4 Solve')
     deconvolved = xp.linalg.solve(iCov, ATNinv.dot(pixel_values))
+    safe_range_pop(xp)
     return deconvolved, iCov
 
 def xp_decorrelate(iCov):
@@ -66,7 +79,7 @@ def xp_decorrelate(iCov):
     # Check BS eqn.14
     assert xp.allclose(iCov, R.T.dot(xp.diag(ivar).dot(R)))
     return ivar, R
-    
+
 
 def xp_decorrelate_blocks(iCov, block_size):
     """Calculate the decorrelated errors and resolution matrix via BS Eq 19
@@ -83,12 +96,17 @@ def xp_decorrelate_blocks(iCov, block_size):
     size = iCov.shape[0]
     assert size % block_size == 0
     #- Invert iCov (B&S eq 17)
+    safe_range_push(xp, 'iCov eigh decompose')
     u, v = xp.linalg.eigh((iCov + iCov.T)/2.)
+    safe_range_pop(xp)
     assert xp.all(u > 0), 'Found some negative iCov eigenvalues.'
     # Check that the eigenvectors are orthonormal so that vt.v = 1
     assert xp.allclose(xp.eye(len(u)), v.T.dot(v))
+    safe_range_push(xp, 'compose C')
     C = (v * (1.0/u)).dot(v.T)
+    safe_range_pop(xp)
     #- Calculate C^-1 = QQ (B&S eq 17-19)
+    safe_range_push(xp, 'C^-1 = QQ by block')
     Q = xp.zeros_like(iCov)
     #- Proceed one block at a time
     for i in range(0, size, block_size):
@@ -100,10 +118,13 @@ def xp_decorrelate_blocks(iCov, block_size):
         assert xp.allclose(xp.eye(len(bu)), bv.T.dot(bv))
         bQ = (bv * xp.sqrt(1.0/bu)).dot(bv.T)
         Q[s] = bQ
+    safe_range_pop(xp)
     #- Calculate the corresponding resolution matrix and diagonal flux errors. (BS Eq 11-13)
+    safe_range_push(xp, 'resolution and ivar')
     s = xp.sum(Q, axis=1)
     R = Q/s[:, xp.newaxis]
     ivar = s**2
+    safe_range_pop(xp)
     #- Check BS eqn.14
     assert xp.allclose(Q.dot(Q), R.T.dot(xp.diag(ivar).dot(R)))
     return ivar, R
@@ -122,6 +143,8 @@ def xp_ex2d_patch(img, ivar, A4, decorrelate='signal'):
         R (nspec*nwave, nspec*nwave): dense resolution matrix
     """
     # timer = Timer()
+    xp = get_array_module(A4)
+    safe_range_push(xp, 'patch init')
     assert decorrelate in ('signal', 'noise')
     ny, nx, nspec, nwave = A4.shape
     assert img.shape == (ny, nx)
@@ -129,21 +152,28 @@ def xp_ex2d_patch(img, ivar, A4, decorrelate='signal'):
     pixel_values = img.ravel()
     pixel_ivar = ivar.ravel()
     A = A4.reshape(ny*nx, nspec*nwave)
+    safe_range_pop(xp)
     # timer.split('init')
     # Deconvole fiber traces
+    safe_range_push(xp, 'deconvolve')
     deconvolved, iCov = xp_deconvolve(pixel_values, pixel_ivar, A)
+    safe_range_pop(xp)
     # timer.split('deconvolve')
     # Calculate the decorrelated errors and resolution matrix.
+    safe_range_push(xp, 'decorrelate')
     if decorrelate == 'signal':
         fluxivar, resolution = xp_decorrelate_blocks(iCov, nwave)
     elif decorrelate == 'noise':
         fluxivar, resolution = xp_decorrelate(iCov)
     else:
         raise ValueError(f'{decorrelate} is not a valid value for decorrelate')
+    safe_range_pop(xp)
     # timer.split('decorrelate')
     # Convolve the reduced flux (BS eq 16)
+    safe_range_push(xp, 'reconvolve')
     flux = resolution.dot(deconvolved).reshape(nspec, nwave)
     fluxivar = fluxivar.reshape(nspec, nwave)
+    safe_range_pop(xp)
     # timer.split('reconvolve')
     # timer.print_splits()
     return flux, fluxivar, resolution
