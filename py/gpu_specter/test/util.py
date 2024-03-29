9a23369dea7d523d955d07181de37a660d097d32
Stephen Bailey <StephenBailey@lbl.gov>
2024-03-28 17:36:16 -0700

    test input based on public fuji not daily

--- py/gpu_specter/test/util.py
+++ py/gpu_specter/test/util.py
@@ -11,20 +11,26 @@ def find_test_file(filetype):
 
     Returns filepath or None if unavailable
     """
-    nerscdir = '/global/cfs/cdirs/desi/spectro/redux/daily'
-    nerscurl = 'https://data.desi.lbl.gov/desi/spectro/redux/daily'
+    ## nerscdir = '/global/cfs/cdirs/desi/spectro/redux/daily'
+    ## nerscurl = 'https://data.desi.lbl.gov/desi/spectro/redux/daily'
+
+    nerscdir = '/global/cfs/cdirs/desi/public/edr/spectro/redux/fuji'
+    nerscurl = 'https://data.desi.lbl.gov/public/edr/spectro/redux/fuji'
+    night = 20201214
+    expid = 67784
+
     if filetype == 'psf':
         #- PSF is small enough to be included with the repo
-        return importlib.resources.files('gpu_specter') / 'test/data/psf-r0-00051060.fits'
+        return importlib.resources.files('gpu_specter') / f'test/data/psf-r0-{expid:08d}.fits'
     elif filetype == 'preproc':
         if 'NERSC_HOST' in os.environ:
-            return f'{nerscdir}/preproc/20200219/00051060/preproc-r0-00051060.fits'
+            return f'{nerscdir}/preproc/{night}/{expid:08d}/preproc-r0-{expid:08d}.fits'
         else:
             #- TODO: download to test/data/ and return that
             return None
     elif filetype == 'frame':
         if 'NERSC_HOST' in os.environ:
-            return f'{nerscdir}/exposures/20200219/00051060/frame-r0-00051060.fits'
+            return f'{nerscdir}/exposures/{night}/{expid:08d}/frame-r0-{expid:08d}.fits'
         else:
             #- TODO: download to test/data/ and return that
             return None
